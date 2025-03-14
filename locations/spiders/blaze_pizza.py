0f3d1eaf93f615fd215b7ac0af0e37f088493cf1
Spencer Alves <impiaaa@gmail.com>
2025-03-14 11:33:58 -0700

    Blaze Pizza: Use the same, simplified endpoint as the Panda Express spider

--- locations/spiders/blaze_pizza.py
+++ locations/spiders/blaze_pizza.py
@@ -7,15 +7,10 @@ class BlazePizzaSpider(scrapy.Spider):
     name = "blaze_pizza"
     item_attributes = {"brand": "Blaze Pizza", "brand_wikidata": "Q23016666"}
     allowed_domains = ["nomnom-prod-api.blazepizza.com"]
-    start_urls = ["https://nomnom-prod-api.blazepizza.com/extras/restaurant/summary/state"]
+    start_urls = ["https://nomnom-prod-api.blazepizza.com/restaurants"]
 
     def parse(self, response):
-        url = "https://nomnom-prod-api.blazepizza.com"
-        for data in response.json().get("data"):
-            for row in data.get("cities"):
-                yield scrapy.Request(url=f'{url}{row.get("datauri")}', callback=self.parse_store)
+        for location in response.json()["restaurants"]:
+            item = DictParser.parse(location)
 
-    def parse_store(self, response):
-        item = DictParser.parse(response.json().get("data")[0].get("restaurants")[0])
-
-        yield item
+            yield item
